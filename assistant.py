35324173eca255370d369e35de430e53bae2a7db
Brandon Schmuck <bschmuck@BRANDONMACBOOK2.WV.CC.CMU.EDU>
2016-01-12 16:14:39 -0500

    Updated with custom commands

--- assistant.py
+++ assistant.py
@@ -7,6 +7,7 @@ import json
 import pyttsx
 import platform
 import os
+import icalendar
 
 CLIENT_ACCESS_TOKEN = 'ae9ba44bfc784edfb047fa865c8e0a0c'
 SUBSCRIPTION_KEY = 'bb578bf5-b17b-4a22-bf0d-4aa2492e0401' 
@@ -17,81 +18,108 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RECORD_SECONDS = 2
 
-def onFinishUtterance(name, isCompleted):
-    print("Finished!")
-
 def main():
-    engine = pyttsx.init()
-    engine.setProperty('rate', 200)
-    voices = engine.setProperty('voice', 'com.apple.speech.synthesis.voice.Alex')
+
+    setupCampusEvents()
+
+    while True:
+        engine = pyttsx.init()
+        engine.setProperty('rate', 200)
+        voices = engine.setProperty('voice', 'com.apple.speech.synthesis.voice.Alex')
 
 
-    resampler = apiai.Resampler(source_samplerate = RATE)
-    vad = apiai.VAD()
-    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN, SUBSCRIPTION_KEY)
-    request = ai.voice_request()
-    request.lang = 'en'
+        resampler = apiai.Resampler(source_samplerate = RATE)
+        vad = apiai.VAD()
+        ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN, SUBSCRIPTION_KEY)
+        request = ai.voice_request()
+        request.lang = 'en'
 
-    def streamCallback(in_data, frame_count, time_info, status):
-        frames, data = resampler.resample(in_data, frame_count)
-        state = vad.processFrame(frames)
-        request.send(data)
+        def streamCallback(in_data, frame_count, time_info, status):
+            frames, data = resampler.resample(in_data, frame_count)
+            state = vad.processFrame(frames)
+            request.send(data)
 
-        if(state == 1):
-            return in_data, pyaudio.paContinue
-        else:
-            return in_data, pyaudio.paComplete
+            if(state == 1):
+                return in_data, pyaudio.paContinue
+            else:
+                return in_data, pyaudio.paComplete
 
-    pyAud = pyaudio.PyAudio()
+        pyAud = pyaudio.PyAudio()
 
-    stream = pyAud.open(format = FORMAT,
-                        channels = CHANNELS,
-                        rate = RATE,
-                        input = True,
-                        output = False,
-                        frames_per_buffer = CHUNK,
-                        stream_callback = streamCallback
-                        )
+        stream = pyAud.open(format = FORMAT,
+                            channels = CHANNELS,
+                            rate = RATE,
+                            input = True,
+                            output = False,
+                            frames_per_buffer = CHUNK,
+                            stream_callback = streamCallback
+                            )
 
-    stream.start_stream()
+        stream.start_stream()
 
-    print("Yo man. Say something!")
+        print("Yo man. Say something!")
 
-    try:
-        while stream.is_active():
-            time.sleep(0.1)
-    except Exception:
-        raise e
-    except KeyboardInterrupt:
-        pass
+        try:
+            while stream.is_active():
+                time.sleep(0.1)
+        except Exception:
+            raise e
+        except KeyboardInterrupt:
+            pass
 
-    stream.stop_stream()
-    stream.close()
-    pyAud.terminate()
+        stream.stop_stream()
+        stream.close()
+        pyAud.terminate()
 
-    print platform.system()
+        print platform.system()
 
 
-    print("Wait for response...")
-    response = request.getresponse()
+        print("Wait for response...")
+        response = request.getresponse()
 
-    jsonString = (response.read()).decode('utf-8')
-    jsonResponse = json.loads(jsonString)
+        jsonString = (response.read()).decode('utf-8')
+        jsonResponse = json.loads(jsonString)
 
-    stringResponse = jsonResponse["result"]["fulfillment"]["speech"]
-    stringResponse = stringResponse.rstrip()
+        stringResponse = jsonResponse["result"]["fulfillment"]["speech"]
+        stringResponse = stringResponse.rstrip()
 
-    if stringResponse is not None and stringResponse != "":
         print(stringResponse)
-        if platform.system() == 'Darwin':
-            os.system("say " + '"' + stringResponse + '"')
+
+        if stringResponse is not None and stringResponse != "":
+            if platform.system() == 'Darwin':
+                os.system("say " + '"' + stringResponse + '"')
+            else:
+                print 'Windows'
+
         else:
-            print 'Windows'
+            print(jsonString)
+            processRequest(jsonResponse["result"]["action"], jsonResponse["result"]["parameters"])
 
-        #engine.say(stringResponse)
-       # engine.runAndWait()
 
-    main()
+def setupCampusEvents():
+    calendar = open('cmu.ics','rb')
+    gcal = icalendar.Calendar.from_ical(calendar.read())
+    for component in gcal.walk():
+        #if component.name == "VEVENT":
+        print component.get('summary')
+        print component.get('dtstart')
+        #print component.get('dtend')
+        #print component.get('dtstamp')
+    calendar.close()
+
+#Processes advanced requests that cannot
+#be handled on api.ai server
+def processRequest(action, parameters):
+
+    speechText = ''
+    if action == "get_free_food":
+        speechText = "Build18 is currently giving out free Chipotle in Hamerschlag Hall."
+    elif action == "get_events":
+        nop
+
+    os.system("say " + '"' + speechText + '"')
+
+
 
 if __name__ == '__main__':
 	main()
